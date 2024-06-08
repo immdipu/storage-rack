da1103cd5d5c3e169aa1bec6ef0c42fe7880f9bc
immdipu <dipuchaurasiya91@gmail.com>
2024-06-08 10:03:22 +0545

    feat(db): add db

---
+++ backend/database/dbsetup.go
@@ -0,0 +1,45 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var (
+	Client *mongo.Client
+	User   *mongo.Collection
+)
+
+func DBset() *mongo.Client {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+
+	client, err := mongo.Connect(ctx, clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Successfully connected to the mongodb")
+
+	Client = client
+
+	return client
+}
+
+func CloseDB() {
+	err := Client.Disconnect(context.Background())
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDb closed.")
+}
