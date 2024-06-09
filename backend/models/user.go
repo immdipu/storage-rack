5f8f015d5ce2d8793ded0ec8470527e8466af76c
immdipu <dipuchaurasiya91@gmail.com>
2024-06-09 08:40:09 +0545

    chore(model): add user model

--- backend/models/user.go
+++ backend/models/user.go
@@ -3,5 +3,11 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type User struct {
-	ID primitive.ObjectID `json:"_id" bson:"_id"`
+	ID             primitive.ObjectID `json:"_id" bson:"_id"`
+	FirstName      string             `json:"firstName" bson:"firstName"`
+	LastName       string             `json:"lastName" bson:"lastName"`
+	Email          string             `json:"email" bson:"email"`
+	Password       string             `json:"password" bson:"password"`
+	Token          string             `json:"token" bson:"token"`
+	SignWithGoogle bool               `json:"signwithgoogle" bson:"signwithgoogle"`
 }
