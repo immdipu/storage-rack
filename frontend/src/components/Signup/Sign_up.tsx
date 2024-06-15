64b2da683efb4796655f7bd3e46c29ee55b7fa11
immdipu <dipuchaurasiya91@gmail.com>
2024-06-15 17:50:57 +0545

    chore: Update CSS and component files for signup page

--- frontend/src/components/Signup/Sign_up.tsx
+++ frontend/src/components/Signup/Sign_up.tsx
@@ -1,9 +1,51 @@
 import React from "react";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import InputWithLabel from "../ui/InputWithLabel";
+import { Button } from "../ui/button";
 
 const Sign_up = () => {
-  return <div></div>;
+  const handleChanges = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event.target.value);
+  };
+
+  return (
+    <div className=" max-w-2xl w-full space-y-5">
+      <InputWithLabel
+        label="Full Name"
+        type="text"
+        value=""
+        onChange={handleChanges}
+        placeholder="Enter your full name"
+        name="full_name"
+      />
+      <InputWithLabel
+        label="Email"
+        type="email"
+        value=""
+        onChange={handleChanges}
+        placeholder="Enter your email address"
+        name="email"
+      />
+      <InputWithLabel
+        label="Password"
+        type="text"
+        value=""
+        onChange={handleChanges}
+        placeholder="Enter your password"
+        name="password"
+      />
+      <InputWithLabel
+        label="Password Confirmation"
+        type="text"
+        value=""
+        onChange={handleChanges}
+        placeholder="Enter your password again"
+        name="password_confirmation"
+      />
+      <Button className="w-full text-white rounded-xl !mt-8 text-base">
+        Sign up
+      </Button>
+    </div>
+  );
 };
 
 export default Sign_up;
--- frontend/src/components/ui/InputWithLabel.tsx
+++ frontend/src/components/ui/InputWithLabel.tsx
@@ -1,12 +1,17 @@
 import React, { type FC } from "react";
 import { Label } from "./label";
 import { Input } from "./input";
+import clsx from "clsx";
 
 interface InputWithLabelProps {
   label: string;
   type: string;
   value: string;
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  placeholder?: string;
+  className?: string;
+  name: string;
+  optional?: boolean;
 }
 
 const InputWithLabel: FC<InputWithLabelProps> = ({
@@ -14,11 +19,27 @@ const InputWithLabel: FC<InputWithLabelProps> = ({
   type,
   onChange,
   value,
+  placeholder,
+  className,
+  name,
+  optional = false,
 }) => {
   return (
     <div>
-      <Label></Label>
-      <Input />
+      <Label className="text-blue-dark pl-1 font-semibold text-base">
+        {label} {!optional && <span className="text-red-500">*</span>}
+      </Label>
+      <Input
+        type={type}
+        onChange={onChange}
+        name={name}
+        value={value}
+        placeholder={placeholder}
+        className={clsx(
+          "rounded-xl placeholder:text-gray mt-2 focus-within:border-neutral-500 duration-200 ease-linear transition-colors outline-none font-normal tracking-wide text-base",
+          className
+        )}
+      />
     </div>
   );
 };
