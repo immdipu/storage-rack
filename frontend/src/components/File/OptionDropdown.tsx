d52f48ef7cf6bafb5da500138c099c4a228d972e
immdipu <dipuchaurasiya91@gmail.com>
2024-06-28 23:07:39 +0545

    feat: Add OptionDropdown component to FileList for additional file options

---
+++ frontend/src/components/File/OptionDropdown.tsx
@@ -0,0 +1,31 @@
+import React, { FC } from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+interface IOptionDropdownProps {
+  children: React.ReactNode;
+}
+
+const OptionDropdown: FC<IOptionDropdownProps> = ({ children }) => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>{children}</DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Profile</DropdownMenuItem>
+        <DropdownMenuItem>Billing</DropdownMenuItem>
+        <DropdownMenuItem>Team</DropdownMenuItem>
+        <DropdownMenuItem>Subscription</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default OptionDropdown;
